676792ff12373607e4943c28cc2898d28470b160
kev26 <huynguyenkev@gmail.com>
2022-01-06 23:56:23 +0700

    upload1

---
+++ app/js/script.js
@@ -0,0 +1,27 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', function() {
+    console.log('click Hamburger')
+
+    if(header.classList.contains('open')){ // Close Hamburger Menu
+        header.classList.remove('open');
+        body.classList.remove('noscroll')
+        fadeElems.forEach(function(element) {
+            element.classList.remove('fade-in')
+            element.classList.add('fade-out');
+        })
+
+    }
+    else{ // Open Hamburger Menu
+        body.classList.add('noscroll')
+        header.classList.add('open');
+        fadeElems.forEach(function(element) {
+            element.classList.remove('fade-out')
+            element.classList.add('fade-in');
+        })
+
+    }
+});
